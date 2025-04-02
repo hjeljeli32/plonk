9f054f6f810a6a32f77f90b44e254304864ef148
Hamza Jeljeli <hamzajeljeli@live.fr>
2025-04-02 21:18:37 +0900

    add product check for rational functions and ites test

--- plonk/src/common/protocols.rs
+++ plonk/src/common/protocols.rs
@@ -199,3 +199,134 @@ pub fn verify_product_check(
         && kzg_verify(gp, com_q, r, q_r, proof_q_r)
         && kzg_verify(gp, com_f, r * w, f_w_r, proof_f_w_r)
 }
+
+// Constructs the polynomials t and t1 based on polynomials f,g and subset Omega
+pub fn compute_t_and_t1_product_check_rational_functions(
+    Omega: &Vec<Fr>,
+    f: &DensePolynomial<Fr>,
+    g: &DensePolynomial<Fr>,
+) -> (DensePolynomial<Fr>, DensePolynomial<Fr>) {
+    let mut rng = ark_std::test_rng();
+
+    // compute evaluations of t over Omega as product of evaluations of f divided by evaluations of g
+    let k = Omega.len();
+    let mut t_y_vals: Vec<Fr> = (0..k)
+        .map(|i| {
+            (0..i + 1)
+                .map(|j| (f.evaluate(&Omega[j]), g.evaluate(&Omega[j])))
+                .map(|(f_eval, g_eval)| f_eval * g_eval.inverse().unwrap())
+                .reduce(|t_eval, eval| t_eval * eval)
+                .unwrap()
+        })
+        .collect();
+    t_y_vals.extend((0..(f.degree() + 1 - k)).map(|_| Fr::rand(&mut rng)));
+    let mut t_x_vals = Omega.clone();
+    t_x_vals.extend((0..(f.degree() + 1 - k)).map(|_| Fr::rand(&mut rng)));
+    // interpolate polynomial t
+    let t = interpolate_polynomial(&t_x_vals, &t_y_vals);
+
+    // t(w*x)
+    let t_w_x = compose_polynomials(
+        &t,
+        &DensePolynomial {
+            coeffs: vec![Fr::ZERO, Omega[1]],
+        },
+    );
+    // g(w*x)
+    let g_w_x = compose_polynomials(
+        &g,
+        &DensePolynomial {
+            coeffs: vec![Fr::ZERO, Omega[1]],
+        },
+    );
+    // f(w*x)
+    let f_w_x = compose_polynomials(
+        &f,
+        &DensePolynomial {
+            coeffs: vec![Fr::ZERO, Omega[1]],
+        },
+    );
+    // t1(x) = t(w*x)g(w*x) - t(x)f(w*x)
+    let t1 = &t_w_x * &g_w_x - &t * &f_w_x;
+
+    (t, t1)
+}
+
+// Generates the proof of product check of rational functions on subset Omega
+pub fn prove_product_check_rational_functions(
+    gp: &GlobalParameters,
+    w: Fr,
+    k: usize,
+    t: &DensePolynomial<Fr>,
+    q: &DensePolynomial<Fr>,
+    f: &DensePolynomial<Fr>,
+    g: &DensePolynomial<Fr>,
+    r: Fr,
+) -> (Fr, G1, Fr, G1, Fr, G1, Fr, G1, Fr, G1, Fr, G1) {
+    // compute t(w^(k-1)) and its proof
+    let (t_w_k_minus_1, proof_t_w_k_minus_1) = kzg_evaluate(gp, t, w.pow([k as u64 - 1]));
+    // compute t(r) and its proof
+    let (t_r, proof_t_r) = kzg_evaluate(gp, t, r);
+    // compute t(w*r) and its proof
+    let (t_w_r, proof_t_w_r) = kzg_evaluate(gp, t, r * w);
+    // compute q(r) and its proof
+    let (q_r, proof_q_r) = kzg_evaluate(gp, q, r);
+    // compute f(w*r) and its proof
+    let (f_w_r, proof_f_w_r) = kzg_evaluate(gp, f, r * w);
+    // compute g(w*r) and its proof
+    let (g_w_r, proof_g_w_r) = kzg_evaluate(gp, g, r * w);
+
+    (
+        t_w_k_minus_1,
+        proof_t_w_k_minus_1,
+        t_r,
+        proof_t_r,
+        t_w_r,
+        proof_t_w_r,
+        q_r,
+        proof_q_r,
+        f_w_r,
+        proof_f_w_r,
+        g_w_r,
+        proof_g_w_r,
+    )
+}
+
+// Verifies the proof of product check of rational functions on subset Omega
+pub fn verify_product_check_rational_functions(
+    gp: &GlobalParameters,
+    w: Fr,
+    k: usize,
+    com_f: G1,
+    com_g: G1,
+    com_q: G1,
+    com_t: G1,
+    r: Fr,
+    t_w_k_minus_1: Fr,
+    proof_t_w_k_minus_1: G1,
+    t_r: Fr,
+    proof_t_r: G1,
+    t_w_r: Fr,
+    proof_t_w_r: G1,
+    q_r: Fr,
+    proof_q_r: G1,
+    f_w_r: Fr,
+    proof_f_w_r: G1,
+    g_w_r: Fr,
+    proof_g_w_r: G1,
+) -> bool {
+    (t_w_k_minus_1 == Fr::ONE)
+        && (t_w_r * g_w_r - t_r * f_w_r == q_r * (r.pow([k as u64]) - Fr::ONE))
+        && kzg_verify(
+            gp,
+            com_t,
+            w.pow([k as u64 - 1]),
+            t_w_k_minus_1,
+            proof_t_w_k_minus_1,
+        )
+        && kzg_verify(gp, com_t, r, t_r, proof_t_r)
+        && kzg_verify(gp, com_t, r * w, t_w_r, proof_t_w_r)
+        && kzg_verify(gp, com_q, r, q_r, proof_q_r)
+        && kzg_verify(gp, com_f, r * w, f_w_r, proof_f_w_r)
+        && kzg_verify(gp, com_g, r * w, g_w_r, proof_g_w_r)
+}
--- plonk/tests/protocols_tests.rs
+++ plonk/tests/protocols_tests.rs
@@ -1,11 +1,11 @@
 use ark_bls12_381::Fr;
 use ark_ff::{AdditiveGroup, BigInteger, BigInteger256, FftField, Field, PrimeField, UniformRand};
 use ark_poly::{Polynomial, univariate::DensePolynomial};
+use ark_std::rand::seq::SliceRandom;
 use plonk::common::{
     kzg::{kzg_commit, kzg_setup},
     protocols::{
-        compute_q_zero_test, compute_t_and_t1_product_check, prove_equality, prove_product_check,
-        prove_zero_test, verify_equality, verify_product_check, verify_zero_test,
+        compute_q_zero_test, compute_t_and_t1_product_check, compute_t_and_t1_product_check_rational_functions, prove_equality, prove_product_check, prove_product_check_rational_functions, prove_zero_test, verify_equality, verify_product_check, verify_product_check_rational_functions, verify_zero_test
     },
     univariate_polynomials::{interpolate_polynomial, random_polynomial},
 };
@@ -336,7 +336,7 @@ fn test_product_check_fail() {
     // Prover computes quotient polynomial of t1 by Z_Omega
     let q = compute_q_zero_test(k, &t1);
 
-    // check that t1 is divisble by Z_Omega
+    // check that t1 is not divisble by Z_Omega
     assert_ne!(&q * &Z_Omega, t1, "t1 must be not divisible by Z_Omega");
 
     // Prover computes commitment of t
@@ -384,5 +384,234 @@ fn test_product_check_fail() {
             proof_f_w_r,
         ) == false,
         "Verify must return false because polynomial's product over Omega is not equal to 1"
+    );
+}
+
+#[test]
+fn test_product_check_rational_functions_success() {
+    let mut rng = ark_std::test_rng();
+    // this degree is used for kzg setup. We need to commit to polynomial q of degree 12.
+    let degree = 12;
+
+    // generate global parameters
+    let gp = kzg_setup(degree);
+
+    // define omega as element of order 8
+    let mut exponent = Fr::MODULUS;
+    exponent.sub_with_borrow(&BigInteger256::from(1_u64));
+    exponent >>= 3; // divide exponent by 8
+    let omega = Fr::GENERATOR.pow(exponent.0.to_vec());
+    let Omega: Vec<Fr> = (0..8).map(|i| omega.pow([i as u64])).collect();
+
+    // compute x_vals and f_y_vals,g_y_vals
+    let mut x_vals = Omega.clone();
+    x_vals.extend((0..3).map(|_| Fr::rand(&mut rng)));
+    let mut f_y_vals: Vec<Fr> = (0..8).map(|_| Fr::rand(&mut rng)).collect();
+    // set g(w^0)..g(w^k-1) as a permutation of f(w^0)..f(w^k-1)
+    let mut g_y_vals = f_y_vals.clone();
+    g_y_vals.shuffle(&mut rng);
+    f_y_vals.extend((0..3).map(|_| Fr::rand(&mut rng)));
+    g_y_vals.extend((0..3).map(|_| Fr::rand(&mut rng)));
+    // interpolate f,g
+    let f = interpolate_polynomial(&x_vals, &f_y_vals);
+    let g = interpolate_polynomial(&x_vals, &f_y_vals);
+
+    assert_eq!(
+        Omega
+            .iter()
+            .map(|omega| (f.evaluate(&omega), g.evaluate(&omega)))
+            .map(|(f_eval, g_eval)| f_eval * g_eval.inverse().unwrap())
+            .reduce(|prod, eval| prod * eval)
+            .unwrap(),
+        Fr::ONE,
+        "product of f/g over Omega must be equal to 1"
+    );
+
+    // check that f,g are of degree 10
+    assert_eq!(f.degree(), 10, "f must be of degree 10");
+    assert_eq!(g.degree(), 10, "g must be of degree 10");
+
+    // construct Z_Omega (vanishing polynomial) of subset Omega of size 8
+    let k = 8;
+    let mut coefficients = vec![Fr::from(-1)];
+    coefficients.extend(vec![Fr::ZERO; k - 1]);
+    coefficients.push(Fr::ONE);
+    let Z_Omega = DensePolynomial {
+        coeffs: coefficients,
+    }; // -1 + x^8
+
+    // Prover computes commitments of f,g
+    let com_f = kzg_commit(&gp, &f).unwrap();
+    let com_g = kzg_commit(&gp, &g).unwrap();
+
+    // Prover constructs the polynomials t and t1 based on polynomials f,g and subset Omega
+    let (t, t1) = compute_t_and_t1_product_check_rational_functions(&Omega, &f, &g);
+
+    // Prover computes quotient polynomial of t1 by Z_Omega
+    let q = compute_q_zero_test(k, &t1);
+
+    // check that t1 is divisble by Z_Omega
+    assert_eq!(&q * &Z_Omega, t1, "t1 must be divisible by Z_Omega");
+
+    // Prover computes commitment of t
+    let com_t = kzg_commit(&gp, &t).unwrap();
+
+    // Prover computes commitment of q
+    let com_q = kzg_commit(&gp, &q).unwrap();
+
+    // Verifier generates randomly r
+    let r = Fr::rand(&mut rng);
+
+    // Prover proves Product Check of rational functions
+    let (
+        t_w_k_minus_1,
+        proof_t_w_k_minus_1,
+        t_r,
+        proof_t_r,
+        t_w_r,
+        proof_t_w_r,
+        q_r,
+        proof_q_r,
+        f_w_r,
+        proof_f_w_r,
+        g_w_r,
+        proof_g_w_r,
+    ) = prove_product_check_rational_functions(&gp, Omega[1], k, &t, &q, &f, &g, r);
+
+    // Verifier verifies Product Check of rational functions
+    assert!(
+        verify_product_check_rational_functions(
+            &gp,
+            Omega[1],
+            k,
+            com_f,
+            com_g,
+            com_q,
+            com_t,
+            r,
+            t_w_k_minus_1,
+            proof_t_w_k_minus_1,
+            t_r,
+            proof_t_r,
+            t_w_r,
+            proof_t_w_r,
+            q_r,
+            proof_q_r,
+            f_w_r,
+            proof_f_w_r,
+            g_w_r,
+            proof_g_w_r,
+        ),
+        "Verify must return true because rational function's product over Omega is 1"
+    );
+}
+
+#[test]
+fn test_product_check_rational_functions_fail() {
+    let mut rng = ark_std::test_rng();
+    // this degree is used for kzg setup. We need to commit to polynomial q of degree 12.
+    let degree = 12;
+
+    // generate global parameters
+    let gp = kzg_setup(degree);
+
+    // define omega as element of order 8
+    let mut exponent = Fr::MODULUS;
+    exponent.sub_with_borrow(&BigInteger256::from(1_u64));
+    exponent >>= 3; // divide exponent by 8
+    let omega = Fr::GENERATOR.pow(exponent.0.to_vec());
+    let Omega: Vec<Fr> = (0..8).map(|i| omega.pow([i as u64])).collect();
+
+    // generate f,g randomly so the product of their rational function over Omega is not equal to 1
+    let f = random_polynomial(&mut rng, 10);
+    let g = random_polynomial(&mut rng, 10);
+
+    assert_ne!(
+        Omega
+            .iter()
+            .map(|omega| (f.evaluate(&omega), g.evaluate(&omega)))
+            .map(|(f_eval, g_eval)| f_eval * g_eval.inverse().unwrap())
+            .reduce(|prod, eval| prod * eval)
+            .unwrap(),
+        Fr::ONE,
+        "product of f/g over Omega must be not equal to 1"
+    );
+
+    // check that f,g are of degree 10
+    assert_eq!(f.degree(), 10, "f must be of degree 10");
+    assert_eq!(g.degree(), 10, "g must be of degree 10");
+
+    // construct Z_Omega (vanishing polynomial) of subset Omega of size 8
+    let k = 8;
+    let mut coefficients = vec![Fr::from(-1)];
+    coefficients.extend(vec![Fr::ZERO; k - 1]);
+    coefficients.push(Fr::ONE);
+    let Z_Omega = DensePolynomial {
+        coeffs: coefficients,
+    }; // -1 + x^8
+
+    // Prover computes commitments of f,g
+    let com_f = kzg_commit(&gp, &f).unwrap();
+    let com_g = kzg_commit(&gp, &g).unwrap();
+
+    // Prover constructs the polynomials t and t1 based on polynomials f,g and subset Omega
+    let (t, t1) = compute_t_and_t1_product_check_rational_functions(&Omega, &f, &g);
+
+    // Prover computes quotient polynomial of t1 by Z_Omega
+    let q = compute_q_zero_test(k, &t1);
+
+    // check that t1 is not divisble by Z_Omega
+    assert_ne!(&q * &Z_Omega, t1, "t1 must be not divisible by Z_Omega");
+
+    // Prover computes commitment of t
+    let com_t = kzg_commit(&gp, &t).unwrap();
+
+    // Prover computes commitment of q
+    let com_q = kzg_commit(&gp, &q).unwrap();
+
+    // Verifier generates randomly r
+    let r = Fr::rand(&mut rng);
+
+    // Prover proves Product Check of rational functions
+    let (
+        t_w_k_minus_1,
+        proof_t_w_k_minus_1,
+        t_r,
+        proof_t_r,
+        t_w_r,
+        proof_t_w_r,
+        q_r,
+        proof_q_r,
+        f_w_r,
+        proof_f_w_r,
+        g_w_r,
+        proof_g_w_r,
+    ) = prove_product_check_rational_functions(&gp, Omega[1], k, &t, &q, &f, &g, r);
+
+    // Verifier verifies Product Check of rational functions
+    assert!(
+        verify_product_check_rational_functions(
+            &gp,
+            Omega[1],
+            k,
+            com_f,
+            com_g,
+            com_q,
+            com_t,
+            r,
+            t_w_k_minus_1,
+            proof_t_w_k_minus_1,
+            t_r,
+            proof_t_r,
+            t_w_r,
+            proof_t_w_r,
+            q_r,
+            proof_q_r,
+            f_w_r,
+            proof_f_w_r,
+            g_w_r,
+            proof_g_w_r,
+        ) == false,
+        "Verify must return false because rational function's product over Omega is not equal to 1"
     );
 }
