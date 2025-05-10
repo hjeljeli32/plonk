5f078b42e02e253b3e4d8a205fc7f53caafa18c2
Hamza Jeljeli <hamzajeljeli@live.fr>
2025-05-10 11:46:49 +0900

    complete part1 of Prover

--- plonk/src/prover/part1.rs
+++ plonk/src/prover/part1.rs
@@ -1,4 +1,7 @@
-use crate::common::utils::construct_Omega;
+use ark_bls12_381::Fr;
+use ark_poly::Polynomial;
+
+use crate::common::{polynomials::interpolate_polynomial, utils::construct_Omega};
 
 pub fn run() -> () {
     println!("Executing part 1...");
@@ -10,5 +13,57 @@ pub fn run() -> () {
 
     // Define Omega as subgroup of size d
     let Omega = construct_Omega(d);
+    assert_eq!(Omega.len(), d, "Omega must be of length d");
+
+    let (mut x_vals, mut y_vals) = (vec![], vec![]);
+    
+    // T encodes all inputs: T(w^-j) = input#j
+    // T(w^-1) = 5
+    x_vals.push(Omega[d - 1]);
+    y_vals.push(Fr::from(5));
+    // T(w^-2) = 6
+    x_vals.push(Omega[d - 2]);
+    y_vals.push(Fr::from(6));
+    // T(w^-3) = 1
+    x_vals.push(Omega[d - 3]);
+    y_vals.push(Fr::from(1));
+
+    // T encodes all wires of the gates
+    // Gate 0 (Addition)
+    // T(w^0) = 5
+    x_vals.push(Omega[0]);
+    y_vals.push(Fr::from(5));
+    // T(w^1) = 6
+    x_vals.push(Omega[1]);
+    y_vals.push(Fr::from(6));
+    // T(w^2) = 11
+    x_vals.push(Omega[2]);
+    y_vals.push(Fr::from(11));
+
+    // Gate 1 (Addition)
+    // T(w^3) = 6
+    x_vals.push(Omega[3]);
+    y_vals.push(Fr::from(6));
+    // T(w^4) = 1
+    x_vals.push(Omega[4]);
+    y_vals.push(Fr::from(1));
+    // T(w^5) = 7
+    x_vals.push(Omega[5]);
+    y_vals.push(Fr::from(7));
+
+    // Gate 2 (Multiplication)
+    // T(w^6) = 11
+    x_vals.push(Omega[6]);
+    y_vals.push(Fr::from(11));
+    // T(w^7) = 7
+    x_vals.push(Omega[7]);
+    y_vals.push(Fr::from(7));
+    // T(w^8) = 77
+    x_vals.push(Omega[8]);
+    y_vals.push(Fr::from(77));
+
+    // Interpolate the polynomial T that enodes the entire trace
+    let T = interpolate_polynomial(&x_vals, &y_vals);
+    assert_eq!(T.degree(), d-1, "T must be of degree d-1");
 
 }
